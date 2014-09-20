494be5d44485ad04c5371bbde1715deb7d42a4ea
Jongmin Yoon <blueguy@gnu.org>
2014-09-20 14:39:29 +0900

    Add intent feature

--- wear/src/main/java/eager/oppa/choensurrr/com/oppaeager/PasswordShowActivity.java
+++ wear/src/main/java/eager/oppa/choensurrr/com/oppaeager/PasswordShowActivity.java
@@ -1,6 +1,7 @@
 package eager.oppa.choensurrr.com.oppaeager;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
@@ -8,17 +9,38 @@ import android.widget.TextView;
 public class PasswordShowActivity extends Activity {
 
     private TextView mTextView;
+    private TextView firstNumber;
+    private TextView secondNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_show);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
+
+                Intent intent = getIntent();
+                Bundle bundle = intent.getExtras();
+
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView.setText(bundle.getString("bank"));
+
+                String password = bundle.getString("password");
+
+                String s1 = password.substring(0, 2);
+                String s2 = password.substring(2, 4);
+
+                firstNumber = (TextView) stub.findViewById(R.id.firstNumber);
+                firstNumber.setText(s1);
+                secondNumber = (TextView) stub.findViewById(R.id.secondNumber);
+                secondNumber.setText(s2);
+
             }
         });
+
     }
 }
---
+++ wear/src/main/java/eager/oppa/choensurrr/com/oppaeager/keyListenerService.java
@@ -0,0 +1,34 @@
+package eager.oppa.choensurrr.com.oppaeager;
+
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by blueguy on 14. 9. 20.
+ */
+public class keyListenerService extends WearableListenerService {
+
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.i("test", "onMessageReceived()");
+        if(messageEvent.getPath().equals("bank/securecode")) {
+            final String message = new String(messageEvent.getData());
+            int pos = message.indexOf('/');
+
+            String bank = message.substring(0, pos);
+            String password = message.substring(pos);
+            Intent intent = new Intent(this, PasswordShowActivity.class);
+            intent.putExtra("bank", bank);
+            intent.putExtra("password", password);
+            startActivity(intent);
+
+        } else {
+            super.onMessageReceived(messageEvent);
+        }
+    }
+}
