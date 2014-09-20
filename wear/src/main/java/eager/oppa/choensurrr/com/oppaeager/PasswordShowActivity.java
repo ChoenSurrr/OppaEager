911cb87364859d9a2d22599b831819939d69d291
Young-Ho Cha <ganadist@gmail.com>
2014-09-20 15:09:35 +0900

    wear: refactor show contents
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- wear/src/main/java/eager/oppa/choensurrr/com/oppaeager/PasswordShowActivity.java
+++ wear/src/main/java/eager/oppa/choensurrr/com/oppaeager/PasswordShowActivity.java
@@ -12,6 +12,9 @@ public class PasswordShowActivity extends Activity {
     private TextView firstNumber;
     private TextView secondNumber;
 
+    private boolean mLayoutInflateCompleted = false;
+    private boolean mResumed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,25 +25,44 @@ public class PasswordShowActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-
-                Intent intent = getIntent();
-                Bundle bundle = intent.getExtras();
-
-                mTextView = (TextView) stub.findViewById(R.id.text);
-                mTextView.setText(bundle.getString("bank"));
-
-                String password = bundle.getString("password");
-
-                String s1 = password.substring(0, 2);
-                String s2 = password.substring(2, 4);
-
-                firstNumber = (TextView) stub.findViewById(R.id.firstNumber);
-                firstNumber.setText(s1);
-                secondNumber = (TextView) stub.findViewById(R.id.secondNumber);
-                secondNumber.setText(s2);
-
+                mLayoutInflateCompleted = true;
+                updateView();
             }
         });
 
+    }
+
+    private void updateView() {
+        if (mResumed && mLayoutInflateCompleted) {
+            Intent intent = getIntent();
+            Bundle bundle = intent.getExtras();
+
+            final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+            mTextView = (TextView) stub.findViewById(R.id.text);
+            mTextView.setText(bundle.getString("bank"));
+
+            String password = bundle.getString("password");
+
+            String s1 = password.substring(0, 2);
+            String s2 = password.substring(2, 4);
+
+            firstNumber = (TextView) stub.findViewById(R.id.firstNumber);
+            firstNumber.setText(s1);
+            secondNumber = (TextView) stub.findViewById(R.id.secondNumber);
+            secondNumber.setText(s2);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mResumed = true;
+        updateView();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mResumed = false;
     }
 }
